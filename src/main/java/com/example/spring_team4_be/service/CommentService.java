a5f40da55952ba58770c092dd17318ff8e217b35
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2022-09-12 00:27:33 +0900

    답글 작성

--- src/main/java/com/example/spring_team4_be/service/CommentService.java
+++ src/main/java/com/example/spring_team4_be/service/CommentService.java
@@ -3,10 +3,12 @@ package com.example.spring_team4_be.service;
 
 import com.example.spring_team4_be.dto.ResponseDto;
 import com.example.spring_team4_be.dto.TwitRequestDto;
+import com.example.spring_team4_be.dto.TwitResponseDto;
 import com.example.spring_team4_be.entity.Member;
 import com.example.spring_team4_be.entity.Twit;
 import com.example.spring_team4_be.repository.TwitRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,22 +17,45 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class CommentService {
     private final TwitRepository twitRepository;
     private final ReTwitService reTwitService;
     @Transactional
-    public ResponseDto<?> create(Long id, TwitRequestDto requestDto, HttpServletRequest request){
+    public ResponseDto<?> create(Long twit_id, TwitRequestDto requestDto, HttpServletRequest request){
         Member member = reTwitService.validateMember(request);
 
         if(member == null)
             return ResponseDto.fail("INVALID_TOKEN", "토큰이 유효하지 않습니다.");
 
-        Optional<Twit> parentTwit = twitRepository.findById(id);
+        if(isPresentTwit(twit_id) == null) {
+            return ResponseDto.fail("NOT_FOUNT_TWIT", "트윗을 찾을 수 없습니다.");
+        }
+        Twit twit = Twit.builder()
+                .content(requestDto.getContent())
+                .url(requestDto.getUrl())
+                .reTwit(twit_id)
+                .member(member)
+                .build();
+        twitRepository.save(twit);
 
 
+        return ResponseDto.success(
+                TwitResponseDto.builder()
+                        .id(twit.getId())
+                        .userFrofileImage(member.getImageUrl())
+                        .nickname(member.getNickname())
+                        .userId(member.getUsername())
+                        .content(twit.getContent())
+                        .fileUrl(twit.getUrl())
+                        .createdAt(twit.getCreatedAt())
+                        .build()
+        );
+    }
 
-
-
-        return ResponseDto.success("");
+    @Transactional(readOnly = true)
+    public Twit isPresentTwit(Long id) {
+        Optional<Twit> optionalBook_review = twitRepository.findById(id);
+        return optionalBook_review.orElse(null);
     }
 }
