559febf45c9bbc251b5be38ef495d59e6cf20d74
Hyemm <hmlee5938@gmail.com>
2022-09-12 05:17:18 +0900

    Create PublicMethod.java

---
+++ src/main/java/com/example/spring_team4_be/util/PublicMethod.java
@@ -0,0 +1,45 @@
+package com.example.spring_team4_be.util;
+
+import com.example.spring_team4_be.dto.reponse.ResponseDto;
+import com.example.spring_team4_be.entity.Member;
+import com.example.spring_team4_be.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PublicMethod {
+    private final TokenProvider tokenProvider;
+
+    @Transactional
+    public ResponseDto<?> checkLogin(HttpServletRequest request){
+        if (null == request.getHeader("Refresh-Token")) {
+            return ResponseDto.fail("MEMBER_NOT_FOUND","로그인이 필요합니다.");
+        }
+
+        if (null == request.getHeader("Authorization")) {
+            return ResponseDto.fail("MEMBER_NOT_FOUND","로그인이 필요합니다.");
+        }
+
+        Member member = validateMember(request);
+        if (null == member) {
+            return ResponseDto.fail("INVALID_TOKEN","토큰이 유효하지 않습니다.");
+        }
+
+        return ResponseDto.success(member);
+    }
+
+    @Transactional
+    public Member validateMember(HttpServletRequest request) {
+        if (!tokenProvider.validateToken(request.getHeader("Refresh-Token"))) {
+            return null;
+        }
+        return tokenProvider.getMemberFromAuthentication();
+    }
+}
