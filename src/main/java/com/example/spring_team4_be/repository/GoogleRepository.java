e110555d5d56218c3bf6d646a56197df25c4feb6
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2022-09-15 02:31:06 +0900

    구글로그인

--- src/main/java/com/example/spring_team4_be/repository/GoogleRepository.java
+++ src/main/java/com/example/spring_team4_be/repository/GoogleRepository.java
@@ -3,9 +3,9 @@ package com.example.spring_team4_be.repository;
 import com.example.spring_team4_be.entity.Google;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface GoogleRepository extends JpaRepository<Google,Long> {
-    List<Google> findByEmail(String Email);
+    Optional<Google> findByEmail(String Email);
 
 }
